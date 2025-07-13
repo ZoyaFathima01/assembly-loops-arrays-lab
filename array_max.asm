b0caa1eca3388395e74a8e4dbd7acab0a15791c7
ZoyaFathima01 <zoyaafathimaa001@gmail.com>
2025-07-13 16:46:40 -0700

    Update array_max.asm

--- array_max.asm
+++ array_max.asm
@@ -1,22 +1,30 @@
+section .data
+array dd 12, 7, 25       ; Array with 3 integers (length = 3)
+len   equ 3              ; Length = 3
+
 section .text
-    global _start
+global _start
 
 _start:
-    mov esi, array      ; address of array start
-    mov eax, [esi]      ; current max = first element
-    mov ecx, 2          ; counter for two more elements
+    mov ecx, len         ; Set loop counter
+    mov esi, array       ; ESI points to the array
+    mov eax, [esi]       ; Assume first value is the largest
+    add esi, 4           ; Move to second element
+    dec ecx              ; One element checked
 
-next_element:
-    add esi, 4
-    mov ebx, [esi]
-    cmp eax, ebx
-    jge skip            ; if current max >= ebx, skip
-    mov eax, ebx        ; else update max
+find_largest:
+    cmp ecx, 0
+    je done
+    mov ebx, [esi]       ; Load current element
+    cmp ebx, eax
+    jle skip             ; If smaller, skip
+    mov eax, ebx         ; Update largest
 skip:
-    loop next_element
+    add esi, 4
+    dec ecx
+    jmp find_largest
 
-    mov eax, 1
+done:
+    mov ebx, eax         ; Store result (largest) in EBX
+    mov eax, 1           ; syscall: exit
     int 0x80
-
-section .data
-    array dd 5, 10, 7
