f2658fccef4bd6942f16d1d7cdf4589b7634254a
ZoyaFathima01 <zoyaafathimaa001@gmail.com>
2025-07-12 23:23:43 -0700

    Create counter_ebx.asm

---
+++ counter_ebx.asm
@@ -0,0 +1,12 @@
+section .text
+    global _start
+
+_start:
+    mov ebx, 0      ; EBX is counter, start at 0
+label:
+    inc ebx         ; increment EBX
+    cmp ebx, 10     ; compare EBX to 10
+    jl label        ; loop if EBX < 10
+
+    mov eax, 1      ; exit syscall
+    int 0x80
