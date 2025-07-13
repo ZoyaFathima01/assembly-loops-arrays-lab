1c1d96638ef1f5ab62b0acdbffdec34adfafd29b
ZoyaFathima01 <zoyaafathimaa001@gmail.com>
2025-07-13 16:59:10 -0700

    Update counter_ebx.asm

--- counter_ebx.asm
+++ counter_ebx.asm
@@ -2,11 +2,13 @@ section .text
     global _start
 
 _start:
-    mov ebx, 0      ; EBX is the counter, start at 0
-label:
-    inc ebx         ; increment EBX
-    cmp ebx, 10     ; compare EBX to 10
-    jl label        ; loop if EBX < 10
+    mov ebx, 10        ; EBX will act as our counter
+    mov eax, 0         ; Just a dummy register to test loop
 
-    mov eax, 1      ; exit syscall
+counter_loop:
+    inc eax            ; Increase EAX to simulate work being done
+    dec ebx            ; Decrement EBX manually
+    jnz counter_loop   ; If EBX ≠ 0, repeat the loop
+
+    mov eax, 1         ; syscall: exit
     int 0x80
