95191963ca0eb36b9c32dcb23dd29442da33cc0b
ZoyaFathima01 <zoyaafathimaa001@gmail.com>
2025-07-13 01:26:09 -0700

    Update fibonacci.asm

--- fibonacci.asm
+++ fibonacci.asm
@@ -2,18 +2,19 @@ section .text
     global _start
 
 _start:
-    mov eax, 0      ; fib0
-    mov ebx, 1      ; fib1
-    mov ecx, 10     ; counter for 10 Fibonacci numbers
-    mov edx, 0      ; accumulator for sum
+    mov eax, 0      ; fib0 = 0
+    mov ebx, 1      ; fib1 = 1
+    mov ecx, 10     ; count 10 Fibonacci numbers
+    mov edx, 0      ; accumulator for sum (optional)
 
 loop_fib:
-    add edx, eax    ; add fib0 to sum
-    mov esi, eax    ; save fib0
-    add eax, ebx    ; fib0 = fib0 + fib1
+    add edx, eax    ; add current fib0 to sum
+    mov esi, eax    ; save fib0 temporarily
+    add eax, ebx    ; fib0 = fib0 + fib1 (next fib number)
     mov ebx, esi    ; fib1 = previous fib0
     dec ecx
     jnz loop_fib
 
-    mov eax, 1
+    mov ebx, eax    ; **move final fib number into ebx for exit code**
+    mov eax, 1      ; sys_exit
     int 0x80
